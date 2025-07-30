83f4bec50df5b61766abe02180f4fe94f0eaf716
Zesenem <zesenemproductions@hotmail.com>
2025-07-30 12:39:54 +0100

    cookieconsent

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import { useEffect } from "react";
 import { Outlet, ScrollRestoration } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
+  useEffect(() => {
+    if (window.cookieconsent) {
+      window.cookieconsent.initialise({
+        palette: {
+          popup: {
+            background: "#1E1E24",
+            text: "#EDEFF7",
+          },
+          button: {
+            background: "#6EE7B7",
+            text: "#1E1E24",
+          },
+        },
+        theme: "edgeless",
+        content: {
+          message:
+            "This website uses cookies to ensure you get the best experience on our website.",
+          dismiss: "Got it!",
+          link: "Learn more",
+          href: "/privacy-policy", 
+        },
+      });
+    }
+  }, []); 
+
   return (
     <>
       <ScrollRestoration />
